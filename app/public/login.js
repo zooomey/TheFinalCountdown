a5d455583564b5785533736db054b250bd984fa8
chrisflune <94500466+chrisflune@users.noreply.github.com>
2022-11-12 15:43:54 -0500

    Update login.js
    User login now gives confirmation messages and receives the userID from DB

--- app/public/login.js
+++ app/public/login.js
@@ -1,8 +1,10 @@
 let usernameInput = document.getElementById("username");
 let passwordInput = document.getElementById("password");
 let result = document.getElementById("result");
+let login = document.getElementById("login");
+let create = document.getElementById("create");
 
-document.getElementById("login").addEventListener("click", () => {
+login.addEventListener("click", () => {
 	fetch("/signin", {
 		method: "POST",
 		headers: {
@@ -12,13 +14,31 @@ document.getElementById("login").addEventListener("click", () => {
 			username: usernameInput.value,
 			plaintextPassword: passwordInput.value,
 		})
-	}).then((response) => {
-		if (response.status === 200) {
-			result.textContent = "Login successful";
-			result.classList.remove("error");
-		} else {
-			result.textContent = "Login failed";
-			result.classList.add("error");
-		}
+	}).then((response) => response.json())
+		.then((data) => {
+			if (data.status === 200) {
+				var username = data.username;
+				result.textContent = "Login successful. Welcome " + username + "!";
+				result.classList.remove("error");
+				console.log("Login successful. Welcome ", data.username);
+			} else {
+				result.textContent = "Login failed";
+				result.classList.add("error");
+			}
+		}).catch(error => {
+			console.log(error);
+		});
+});
+
+/*
+//placeholder for function to redirect to dashboard page after user signed-in
+
+create.addEventListener("click", () => {
+	fetch("/create").then(response => {
+
+	})
+	.catch(error => {
+		console.log(error);
 	});
 });
+*/
