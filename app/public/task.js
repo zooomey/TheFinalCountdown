7c8fcbefd70cc749e9c3cda3e6fc4ba23a3888f7
Claire Toomey <cmt392@drexel.edu>
2022-11-12 23:15:30 -0500

    Updates task.js

--- app/public/task.js
+++ app/public/task.js
@@ -8,7 +8,7 @@ button.addEventListener("click", () => {
     }
 
     else{
-        document.querySelector('#tasks').textContent += `
+        document.querySelector('#tasks').innerHTML += `
             <div class="task">
                 <span id="taskname">
                     ${document.querySelector('#newtask input').value}
