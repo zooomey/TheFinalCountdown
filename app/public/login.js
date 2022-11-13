48f643d0d410bea4fa9fb1d5a8792aad1cfbfd3b
chrisflune <94500466+chrisflune@users.noreply.github.com>
2022-11-13 02:49:47 -0500

    Update login.js
    Combined login.js & create.js

--- app/public/login.js
+++ app/public/login.js
@@ -1,46 +1,63 @@
 let usernameInput = document.getElementById("username");
 let passwordInput = document.getElementById("password");
 let result = document.getElementById("result");
-let login = document.getElementById("login");
+let login = document.getElementById("login_button");
 let nav = document.getElementById("SIGNIN");
 let create = document.getElementById("create");
+let swap_nav = document.getElementById("swap_nav");
+let login_nav_text = document.getElementById("login_nav_text");
 
 login.addEventListener("click", () => {
-	fetch("/signin", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json"
-		},
-		body: JSON.stringify({
-			username: usernameInput.value,
-			plaintextPassword: passwordInput.value,
-		})
-	}).then((response) => response.json())
-		.then((data) => {
-			if (data.status === 200) {
-				var username = data.username;
-				result.textContent = "Login successful. Welcome " + username + "!";
-				nav.textContent = "Welcome to The Final Countdown " + username + "!";
+
+	if(swap_nav.className === "create_nav")
+	{
+		fetch("/signin", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				username: usernameInput.value,
+				plaintextPassword: passwordInput.value,
+			})
+		}).then((response) => response.json())
+			.then((data) => {
+				if (data.status === 200) {
+					var username = data.username;
+					result.textContent = "Login successful. Welcome " + username + "!";
+					nav.textContent = "Welcome to The Final Countdown " + username + "!";
+					result.classList.remove("error");
+					console.log("Login successful. Welcome ", data.username);
+				} else {
+					result.textContent = "Login failed";
+					result.classList.add("error");
+				}
+			}).catch(error => {
+				console.log(error);
+			});
+	}
+
+	else if (swap_nav.className === "login_nav")
+	{
+		fetch("/signup", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				username: usernameInput.value,
+				plaintextPassword: passwordInput.value
+			})
+		}).then((response) => {
+			if (response.status === 200) {
+				result.textContent = "Account was created";
+				login_nav_text.textContent = "Please verify account by signing in ";
 				result.classList.remove("error");
-				console.log("Login successful. Welcome ", data.username);
 			} else {
-				result.textContent = "Login failed";
+				result.textContent = "Account creation failed";
 				result.classList.add("error");
 			}
-		}).catch(error => {
-			console.log(error);
 		});
+	}
+
 });
-
-/*
-//placeholder for function to redirect to dashboard page
-
-create.addEventListener("click", () => {
-	fetch("/create").then(response => {
-
-	})
-	.catch(error => {
-		console.log(error);
-	});
-});
-*/
