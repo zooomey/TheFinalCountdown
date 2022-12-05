503882a181424ed1138d4931c6f17325cabf9c1d
chrisflune <94500466+chrisflune@users.noreply.github.com>
2022-12-05 08:51:47 -0500

    Update kanban.js

--- app/public/kanban.js
+++ app/public/kanban.js
@@ -1,4 +1,5 @@
-
+//let trashcans = document.getElementsByClassName("delete_img");
+function refreshKanban() {
   if (cookie){
     fetch("/search/tasks", {
 			method: "POST",
@@ -12,13 +13,56 @@
 		}).then((response) => response.json())
 			.then((data) => {
 				if (data.status === 200) {
-					//console.log(data.rows[0]);
+          document.getElementById('1').textContent="";
+          document.getElementById('2').textContent="";
+          document.getElementById('3').textContent="";
+          document.getElementById('4').textContent="";
 
           // INSERT TASKS INTO TO-DO / IN-PROGRESS / COMPLETED DIVS
+          for (task of data.rows) {
+            if(task.abandoned){
+              let abandoned = document.getElementById('1');
+              var li = document.createElement("li");
+              li.setAttribute("class", "drag-item " + task.taskid + " abandoned");
+              var x = abandoned.appendChild(li);
+            }
+            else if(task.inprogress) {
+              let inprogress = document.getElementById('3');
+              var li = document.createElement("li");
+              li.setAttribute("class", "drag-item " + task.taskid + " inprogress");
+              var x = inprogress.appendChild(li);
+            }
+            else if(task.completed) {
+              let completed = document.getElementById('4');
+              var li = document.createElement("li");
+              li.setAttribute("class", "drag-item " + task.taskid + " completed");
+              var x = completed.appendChild(li);
+            }
+            else {
+              let todo = document.getElementById('2');
+              var li = document.createElement("li");
+              li.setAttribute("class", "drag-item " + task.taskid + " todo");
+              var x = todo.appendChild(li);
+            }
 
+            var table = document.createElement("table");
+            var task_box = x.appendChild(table);
+            task_box.setAttribute("width", "100%");
+            var task_box_row = task_box.insertRow(0);
+            var task_box_name = task_box_row.insertCell(0);
+            var task_box_delete = task_box_row.insertCell(1);
+            task_box_name.textContent = task.taskname;
+            var img = document.createElement("img");
+            task_box_delete.appendChild(img);
+            task_box_delete.setAttribute("style", "text-align: right");
+            img.setAttribute("src", "img/delete.png");
+            img.setAttribute("class", "delete_img");
+            img.setAttribute("onclick", "delete_task(" + task.taskid + ");");
+            x.setAttribute("id", task.taskid);
+          }
         }
         else {
-          //console.log("status: ", data.status);
+          console.log("status: ", data.status);
         }
       });
   }
@@ -27,23 +71,61 @@
   	document.getElementById('1'),
   	document.getElementById('2'),
   	document.getElementById('3'),
+    document.getElementById('4')
   ])
 
   .on('drag', function(el) {
-
   	// add 'is-moving' class to element being dragged
   	el.classList.add('is-moving');
   })
   .on('dragend', function(el) {
-
   	// remove 'is-moving' class from element after dragging has stopped
   	el.classList.remove('is-moving');
-
   	// add the 'is-moved' class for 600ms then remove it
   	window.setTimeout(function() {
   		el.classList.add('is-moved');
-  		window.setTimeout(function() {
+      //console.log("MOVED : taskid ", el.classList[1], " status: ", el.parentElement.id); //taskid
+      fetch("/update_tasks", { //send updates
+  			method: "POST",
+  			headers: {
+  				"Content-Type": "application/json"
+  			},
+  			body: JSON.stringify({
+  				userid: cookie.id,
+  				cookie: cookie.cookie,
+          taskid: el.classList[1],
+          status: el.parentElement.id
+  			})
+  		}).then((response) => {
+          refreshReport();
+        });
+
+      window.setTimeout(function() {
   			el.classList.remove('is-moved');
   		}, 600);
   	}, 100);
   });
+}
+
+if (alreadySignedIn) {
+    refreshKanban();
+}
+
+function delete_task(taskID){
+  fetch("/delete_task", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      userid: cookie.id,
+      cookie: cookie.cookie,
+      taskid: taskID
+    })
+  }).then((response) => {
+    //console.log("PERMANENTLY DELETED");
+      refreshReport();
+      refreshKanban();
+      refreshTaskList();
+    });
+}
