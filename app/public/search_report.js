f5fad31cd7ee664399db0faa9e8ea2fe6eacf7c2
chrisflune <94500466+chrisflune@users.noreply.github.com>
2022-12-05 19:03:19 -0500

    final edit

--- app/public/search_report.js
+++ app/public/search_report.js
@@ -1,7 +1,9 @@
 let task_report = document.getElementById("task_report");
 let session_report = document.getElementById("session_report");
+let totals_estimate = document.getElementById("totals_estimate");
+let totals_work = document.getElementById("totals_work");
 
-let total_time;
+let total_time, estimate_seconds, work_seconds;
 
 function refreshReport(){
 	if (cookie){
@@ -25,6 +27,8 @@ function refreshReport(){
 					task_report.setAttribute("id", "task_report");
 					session_report.setAttribute("id", "session_report");
 					total_time = 0;
+					estimate_seconds = 0;
+					work_seconds = 0;
 					var abandoned = 0;
 					var todo = 0;
 					var inprogress = 0;
@@ -41,34 +45,6 @@ function refreshReport(){
 					var cell3 = row.insertCell(3);
 					task_report.appendChild(table);
 
-					var total = abandoned + todo + inprogress + completed;
-					var abandoned_width = Math.round((abandoned / total) * 100);
-					var todo_width = Math.round((todo / total) * 100);
-					var inprogress_width = Math.round((inprogress / total) * 100);
-					var completed_width = Math.round((completed / total) * 100);
-					if (total == 0){
-						abandoned_width = 0;
-						todo_width = 0;
-						inprogress_width = 0;
-						completed_width = 0;
-					}
-
-					cell0.textContent = abandoned_width + "% Abandoned";
-					cell0.setAttribute("width", abandoned_width + "%");
-					cell0.setAttribute("style", "background-color: #ea8c8c; border-radius: 10px 0px 0px 10px; padding: 10px; font-weight: bold");
-
-					cell1.textContent = todo_width + "% To-Do";
-					cell1.setAttribute("width", todo_width + "%");
-					cell1.setAttribute("style", "background-color: #8cb8ea; padding: 10px; font-weight: bold");
-
-					cell2.textContent = inprogress_width + "% In-Progress";
-					cell2.setAttribute("width", inprogress_width + "%");
-					cell2.setAttribute("style", "background-color: #ffdb5b; padding: 10px; font-weight: bold");
-
-					cell3.textContent = completed_width + "% Completed";
-					cell3.setAttribute("width", completed_width + "%");
-					cell3.setAttribute("style", "background-color: #00B961; border-radius: 0px 10px 10px 0px; padding: 10px; font-weight: bold");
-
 					//task time chart
 					var time_table = document.createElement("table");
 					time_table.setAttribute("width", "100%");
@@ -82,35 +58,33 @@ function refreshReport(){
 					var table_row = time_table.insertRow(1);
 					var table_cell0 = table_row.insertCell(0);
 					var table_cell1 = table_row.insertCell(1);
+					var table_cell2 = table_row.insertCell(2);
 					table_cell0.textContent = "Task Name";
-					table_cell1.textContent = "Total Work Time";
+					table_cell1.textContent = "Estimate";
+					table_cell2.textContent = "Total Work Time";
 					table_cell0.setAttribute("style", "font-weight: bold");
 					table_cell1.setAttribute("style", "font-weight: bold");
+					table_cell2.setAttribute("style", "font-weight: bold");
 
 					for (task of data.rows){
 						var time_row = time_table.insertRow(2);
 						var task_cell = time_row.insertCell(0);
-						var time_cell = time_row.insertCell(1);
+						var estimate_cell = time_row.insertCell(1);
+						var time_cell = time_row.insertCell(2);
 						var seconds = parseInt(task.total);
-						var hrs, mins, secs;
+						var ConvertedTime = [];
+						var ConvertedEstimate = [];
+						var ConvertedWork = [];
+						var EstimateTask = [];
+						var hrs, mins, secs, workhrs, workmins, worksecs;
 
-						if (seconds < 60){
-							hrs = 0;
-							mins = 0;
-							secs = seconds;
-						}
-						else if (seconds < 3600){
-							hrs = 0;
-							mins = Math.round(seconds / 60);
-							secs = seconds - (mins * 60);
-						}
-						else{
-							hrs = Math.round(seconds / 3600);
-							mins = Math.round((seconds - (hrs * 3600)) / 60);
-							secs = seconds - (mins * 60);
-						}
+						ConvertedTime = convertSeconds(seconds);
+						hrs = ConvertedTime['hrs'];
+						mins = ConvertedTime['mins'];
+						secs = ConvertedTime['secs'];
 
 							task_cell.textContent = task.taskname;
+
 							var time_div = document.createElement("div");
 							time_cell.appendChild(time_div);
 							time_cell.setAttribute("style", "white-space: nowrap");
@@ -120,27 +94,31 @@ function refreshReport(){
 								time_width = "";
 							}
 							else {
-								time_width = Math.round((task.total / total_time) * 100);
+								time_width = ((task.total / total_time) * 100).toFixed(2);
 							}
 
 							if(task.abandoned){
 								abandoned++;
 								task_cell.setAttribute("style", "background-color: #ea8c8c; padding: 5px; border-radius: 5px 0 0 5px;");
+								estimate_cell.setAttribute("style", "background-color: #ea8c8c; padding: 5px; font-style: italic");
 								time_div.setAttribute("style", "background-color: #ea8c8c; padding: 5px; border-radius: 0 10px 10px 0; width: " + time_width + "%");
 							}
 							else if (task.inprogress){
 								inprogress = 0;
 								task_cell.setAttribute("style", "background-color: #ffdb5b; padding: 5px; border-radius: 5px 0 0 5px;");
+								estimate_cell.setAttribute("style", "background-color: #ffdb5b; padding: 5px; font-style: italic");
 								time_div.setAttribute("style", "background-color: #ffdb5b; padding: 5px; border-radius: 0 10px 10px 0; width: " + time_width + "%");
 							}
 							else if(task.completed) {
 								completed++;
 								task_cell.setAttribute("style", "background-color: #00B961; padding: 5px; border-radius: 5px 0 0 5px;");
+								estimate_cell.setAttribute("style", "background-color: #00B961; padding: 5px; font-style: italic");
 								time_div.setAttribute("style", "background-color: #00B961; padding: 5px; border-radius: 0 10px 10px 0; width: " + time_width + "%");
 							}
 							else {
 								todo++;
 								task_cell.setAttribute("style", "background-color: #8cb8ea; padding: 5px; border-radius: 5px 0 0 5px;");
+								estimate_cell.setAttribute("style", "background-color: #8cb8ea; padding: 5px; font-style: italic");
 								time_div.setAttribute("style", "background-color: #8cb8ea; padding: 5px; border-radius: 0 10px 10px 0; width: " + time_width + "%");
 							}
 
@@ -158,7 +136,58 @@ function refreshReport(){
 								time_div.setAttribute("style", "background-color: #fff; padding: 5px");
 								time_div_table_cell1.textContent = "";
 							}
+							var total = abandoned + todo + inprogress + completed;
+							var abandoned_width = Math.round((abandoned / total) * 100);
+							var todo_width = Math.round((todo / total) * 100);
+							var inprogress_width = Math.round((inprogress / total) * 100);
+							var completed_width = Math.round((completed / total) * 100);
+							if (total == 0){
+								abandoned_width = 0;
+								todo_width = 0;
+								inprogress_width = 0;
+								completed_width = 0;
+							}
+
+							EstimateTask = convertSeconds(Number(task.estimate));
+							hrs = EstimateTask['hrs'];
+							mins = EstimateTask['mins'];
+							secs = EstimateTask['secs'];
+							estimate_cell.textContent = hrs + " h : " + mins + " m : " + secs + " s";
+
+
+							cell0.textContent = abandoned_width + "% Abandoned";
+							cell0.setAttribute("width", abandoned_width + "%");
+							cell0.setAttribute("style", "background-color: #ea8c8c; border-radius: 10px 0px 0px 10px; padding: 10px; font-weight: bold");
+
+							cell1.textContent = todo_width + "% To-Do";
+							cell1.setAttribute("width", todo_width + "%");
+							cell1.setAttribute("style", "background-color: #8cb8ea; padding: 10px; font-weight: bold");
+
+							cell2.textContent = inprogress_width + "% In-Progress";
+							cell2.setAttribute("width", inprogress_width + "%");
+							cell2.setAttribute("style", "background-color: #ffdb5b; padding: 10px; font-weight: bold");
+
+							cell3.textContent = completed_width + "% Completed";
+							cell3.setAttribute("width", completed_width + "%");
+							cell3.setAttribute("style", "background-color: #00B961; border-radius: 0px 10px 10px 0px; padding: 10px; font-weight: bold");
+
+							estimate_seconds += Number(task.estimate);
+							work_seconds += Number(task.total);
+
 					}
+					ConvertedEstimate = convertSeconds(estimate_seconds);
+					hrs = ConvertedEstimate['hrs'];
+					mins = ConvertedEstimate['mins'];
+					secs = ConvertedEstimate['secs'];
+
+					totals_estimate.textContent = hrs + " h : " + mins + " m : " + secs + " s";
+
+					ConvertedWork = convertSeconds(work_seconds);
+					hrs = ConvertedWork['hrs'];
+					mins = ConvertedWork['mins'];
+					secs = ConvertedWork['secs'];
+
+					totals_work.textContent = hrs + " h : " + mins + " m : " + secs + " s";
 				}
 			}).catch ((error) => {
 				console.log(error); //fetch crashed
@@ -168,4 +197,30 @@ function refreshReport(){
 
 if (alreadySignedIn) {
     refreshReport();
+}
+
+function convertSeconds(seconds){
+	var ConvertedTime = new Object();
+	var hrs, mins, secs;
+
+	if (seconds < 60){
+		hrs = 0;
+		mins = 0;
+		secs = seconds;
+	}
+	else if (seconds < 3600){
+		hrs = 0;
+		mins = Math.floor(seconds / 60);
+		secs = seconds - (mins * 60) - (mins % 60);
+	}
+	else{
+		hrs = Math.floor(seconds / 3600);
+		mins = Math.floor((seconds - (hrs * 3600)) / 60);
+		secs =  seconds - (hrs * 3600) - (mins * 60);
+	}
+	ConvertedTime['hrs'] = hrs;
+	ConvertedTime['mins'] = mins;
+	ConvertedTime['secs'] = secs;
+
+	return ConvertedTime;
 }
